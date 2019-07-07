c9e2e918fc128cd175162b4a46c3d6a5ec25c548
mohit1604 <malikmohit619@gmail.com>
2019-07-07 22:54:52 +0530

    adding production build

---
+++ build/precache-manifest.39a235f6e9f154943a67005531cab8a0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8baf3e551e096f7af01af6f5fad02f9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3875228629e5f190cee1",
+    "url": "/static/css/main.e76cfffe.chunk.css"
+  },
+  {
+    "revision": "71e433c4b74ed80a7152",
+    "url": "/static/js/2.17ca37da.chunk.js"
+  },
+  {
+    "revision": "3875228629e5f190cee1",
+    "url": "/static/js/main.6359a3b0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "004b0e65092c105c22987f4a731ea4c0",
+    "url": "/static/media/VisbyRoundCF-Bold.004b0e65.otf"
+  },
+  {
+    "revision": "5b8d56b299b972d012aedde88f923750",
+    "url": "/static/media/VisbyRoundCF-Bold.5b8d56b2.woff"
+  },
+  {
+    "revision": "f372e9b7b5bee4c14dd9614b309e7c1f",
+    "url": "/static/media/VisbyRoundCF-Bold.f372e9b7.eot"
+  },
+  {
+    "revision": "9c28846c2b76bfa720f5535d28edcff5",
+    "url": "/static/media/VisbyRoundCF-DemiBold.9c28846c.woff"
+  },
+  {
+    "revision": "bb87c4c202afc8907cc461c8594adc33",
+    "url": "/static/media/VisbyRoundCF-DemiBold.bb87c4c2.otf"
+  },
+  {
+    "revision": "e940c8a257946953872a187d5af77cf1",
+    "url": "/static/media/VisbyRoundCF-DemiBold.e940c8a2.eot"
+  },
+  {
+    "revision": "672c7c001ac43e56cbe84f94e164d2b6",
+    "url": "/static/media/VisbyRoundCF-Medium.672c7c00.otf"
+  },
+  {
+    "revision": "c924b6afa958cf06730429c9a60b3f92",
+    "url": "/static/media/VisbyRoundCF-Medium.c924b6af.woff"
+  },
+  {
+    "revision": "fa7a7c005f5d09ebedb38dfaba4d4df6",
+    "url": "/static/media/VisbyRoundCF-Medium.fa7a7c00.eot"
+  },
+  {
+    "revision": "11a9224dccd0d656e8d66d4e702fa48c",
+    "url": "/static/media/VisbyRoundCF-Regular.11a9224d.eot"
+  },
+  {
+    "revision": "15ca16e37ede38ccc34153403235ac38",
+    "url": "/static/media/VisbyRoundCF-Regular.15ca16e3.otf"
+  },
+  {
+    "revision": "6f5699749a144b7eb1eba8a17366dc7e",
+    "url": "/static/media/VisbyRoundCF-Regular.6f569974.woff"
+  }
+]);
