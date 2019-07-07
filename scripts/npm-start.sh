a98130b4caf21e408976210ee2a80a95b1525d03
mohit1604 <malikmohit619@gmail.com>
2019-07-07 23:03:19 +0530

    adding appspec.yml files and scripts

---
+++ scripts/npm-start.sh
@@ -0,0 +1,7 @@
+# these scripts is used with hooks in appspec.yml file
+# if we plan to use hooks and do npm install and npm start in deploy
+# stage this will be useful
+
+#!/bin/bash
+cd /home/ubuntu/shell
+sudo npm start
