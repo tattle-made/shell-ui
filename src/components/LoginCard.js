060146e4e3945b77d690127693cf5c54ec70d4be
mohit1604 <malikmohit619@gmail.com>
2019-07-09 13:53:35 +0530

    login page polishing

--- src/components/LoginCard.js
+++ src/components/LoginCard.js
@@ -6,6 +6,11 @@ import { connect } from "react-redux";
 //actions
 import { loginUser } from "../actions/auth";
 
+//components
+import tattleLogo from "../img/logo_logomark.png";
+import {HeadingThree} from "../reusableComponents/text/HeadingThree";
+import {BodyOne} from "../reusableComponents/text/BodyOne";
+
 class LoginCard extends Component {
   constructor(props) {
     super(props);
@@ -16,9 +21,11 @@ class LoginCard extends Component {
       errors: {}
     };
   }
+
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
+
   onSubmit(e) {
     e.preventDefault();
     const userData = {
@@ -28,65 +35,106 @@ class LoginCard extends Component {
     console.log("submitted");
     this.props.loginUser(userData);
   }
+
   render() {
     return (
-      <div className="login mx-auto mt-5">
-        <div className="login-left" />
-        <div className="login-right">
-          <div className="login-right-heading">
-            <HeadingOne text="Unlock your tools" />
-            <HeadingOne text="to fight fake tools" />
-          </div>
-          <form className="mt-2" onSubmit={this.onSubmit.bind(this)}>
-            <input
-              type="text"
-              name="username"
-              value={this.state.username}
-              placeholder="Enter Username"
-              onChange={this.onChange.bind(this)}
-            />
-            {/* <input
-              type="text"
-              name="email"
-              value={this.state.email}
-              placeholder="Enter Email"
-              onChange={this.onChange.bind(this)}
-            /> */}
-            <br />
-            <input
-              type="text"
-              name="password"
-              value={this.state.password}
-              placeholder="Enter Password"
-              onChange={this.onChange.bind(this)}
-            />
-            <br />
-            <Button variant="color-primary-one" className="mt-4" type="submit">
-              Sign In
-            </Button>
-          </form>
-        </div>
-      </div>
+      <div>
 
-      //   <div class="login">
-      //     <div class="login-left" />
-      //     <div class="login-right">
-      //       <form>
-      //         <p>fs</p>
-      //         <input type="text" placeholder="Enter Email" />
-      //         <br />
-      //         <input type="text" placeholder="Enter Passworfd" />
-      //         <br />
-      //         <button type="button">Sign Inn</button>
-      //       </form>
-      //     </div>
-      //   </div>
+        <div className="login-header">
+          <img src={tattleLogo}/>
+        </div>
+
+        <div className="login mx-auto">
+          <div className="login-left" />
+          <div className="login-right">
+            <div className="login-right-heading">
+              <HeadingOne text="Unlock your tools" />
+              <HeadingOne text="to fight fake tools" />
+            </div>
+            <form className="mt-2" onSubmit={this.onSubmit.bind(this)}>
+              <input
+                type="text"
+                name="username"
+                value={this.state.username}
+                placeholder="Enter Username"
+                onChange={this.onChange.bind(this)}
+              />
+              {/* <input
+                type="text"
+                name="email"
+                value={this.state.email}
+                placeholder="Enter Email"
+                onChange={this.onChange.bind(this)}
+              /> */}
+              <br />
+              <input
+                type="text"
+                name="password"
+                value={this.state.password}
+                placeholder="Enter Password"
+                onChange={this.onChange.bind(this)}
+              />
+              <br />
+              <Button variant="color-primary-one"  className="mt-4 btn-login-size" type="submit">
+                Sign In
+              </Button>
+            </form>
+          </div>
+        </div>
+            
+        <div className="login-footer">
+          <div>
+            <HeadingThree text="Reach Out" />
+            <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+          </div>
+          <div>
+            <HeadingThree text="Social" />
+               <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+          </div>
+          <div>
+            <HeadingThree text="Values" />
+               <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+          </div>
+          <div>
+            <HeadingThree text="Values" />
+               <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+            <BodyOne text="facebook" />
+          </div>
+        </div>
+    </div>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  errors: state.errors
+  errors:state.errors
 });
 
 export default connect(
