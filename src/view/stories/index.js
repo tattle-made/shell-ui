0ecb3685a962da14ceb5fe2dd321d230d8b4f2de
denny <denny@quadrant2.us>
2019-07-17 16:37:09 -0400

    added story book

--- src/view/stories/index.js
+++ src/view/stories/index.js
@@ -5,6 +5,7 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
+import {FilterPostParameters} from '../molecules/search-param';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
@@ -17,3 +18,6 @@ storiesOf('Button', module)
       </span>
     </Button>
   ));
+
+storiesOf('SearchParam', module)
+.add('collapsed', () => <FilterPostParameters/>)
