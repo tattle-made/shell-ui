533b6192ae3022047cb6df2e0b74194e5cd3c00d
mohit1604 <malikmohit619@gmail.com>
2019-07-22 01:34:56 +0530

    searchinput need to fix

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -30,170 +30,171 @@ const options = [
 ];
 
 function SearchInput(props) {
-  const accessibility = SearchInputCapabilities(props.user.role);
-  console.log("accessibility ", accessibility);
+  return <div>I am broke</div>;
+  // // // const accessibility = SearchInputCapabilities(props.user.role);
+  // // console.log("accessibility ", accessibility);
 
-  const [selectedOption, setSelectedOption] = useState("text");
-  const [content_type, setContent_type] = useState([]);
+  // const [selectedOption, setSelectedOption] = useState("text");
+  // const [content_type, setContent_type] = useState([]);
 
-  const handleChange = e => {
-    setSelectedOption(e.value);
-  };
+  // const handleChange = e => {
+  //   setSelectedOption(e.value);
+  // };
 
-  const onSearch = e => {
-    e.preventDefault();
-    props.contentLoading();
-    setTimeout(() => props.search(), 3000);
-    contentLoading();
-  };
+  // const onSearch = e => {
+  //   e.preventDefault();
+  //   props.contentLoading();
+  //   setTimeout(() => props.search(), 3000);
+  //   contentLoading();
+  // };
 
-  const displayResults = cards => {
-    return cards.map(card => (
-      <Card
-        key={card.id}
-        card={card}
-        display={content_type.includes(card.type)}
-      />
-    ));
-  };
+  // const displayResults = cards => {
+  //   return cards.map(card => (
+  //     <Card
+  //       key={card.id}
+  //       card={card}
+  //       display={content_type.includes(card.type)}
+  //     />
+  //   ));
+  // };
 
-  // useEffect(() => {
-  //   console.log("fired");
-  //   // return displayResults(props.fetch.data);
-  // }, [content_type]);
+  // // useEffect(() => {
+  // //   console.log("fired");
+  // //   // return displayResults(props.fetch.data);
+  // // }, [content_type]);
 
-  const checkboxToggle = e => {
-    const new_list = content_type;
-    if (new_list.includes(e)) {
-      new_list.splice(new_list.indexOf(e), 1);
-    } else {
-      new_list.push(e);
-    }
-    setContent_type(new_list);
-  };
+  // const checkboxToggle = e => {
+  //   const new_list = content_type;
+  //   if (new_list.includes(e)) {
+  //     new_list.splice(new_list.indexOf(e), 1);
+  //   } else {
+  //     new_list.push(e);
+  //   }
+  //   setContent_type(new_list);
+  // };
 
-  if (true) {
-    return (
-      <div>
-        <div className="container search-box">
-          <form className="search-form">
-            <div className="form-inline">
-              {/* <Select
-                className="mr-2"
-                placeholder={<FontAwesomeIcon icon={faFileAlt} />}
-                value={selectedOption}
-                onChange={handleChange}
-                options={options}
-              />
-              {selectedOption === "text" ? (
-                <div className="search-content">
-                  <input
-                    className="form-control mr-2"
-                    type="text"
-                    id="search-input"
-                    placeholder="Search"
-                  />
-                </div>
-              ) : selectedOption === "image" ? (
-                <div className="search-content">
-                  <UploadInput label="Upload Image" />
-                </div>
-              ) : (
-                <div className="search-content">
-                  <UploadInput label="Upload Video" />
-                </div>
-              )}
-              <button
-                className="btn search-btn ml-2"
-                type="button"
-                onClick={onSearch}
-              >
-                <FontAwesomeIcon icon={faSearch} color="#fff" />
-              </button>
-            </div> */}
-              <div className="search-input">
-                <div className="search-input-input">
-                  <input type="text" placeholder="Enter Search Term" />
-                  <HeadingTwo text="or" />
-                  <button className="btn  btn-color-white-one">
-                    <FontAwesomeIcon icon={faUpload} /> Upload File
-                  </button>
-                </div>
+  // if (true) {
+  //   return (
+  //     <div>
+  //       <div className="container search-box">
+  //         <form className="search-form">
+  //           <div className="form-inline">
+  //             {/* <Select
+  //               className="mr-2"
+  //               placeholder={<FontAwesomeIcon icon={faFileAlt} />}
+  //               value={selectedOption}
+  //               onChange={handleChange}
+  //               options={options}
+  //             />
+  //             {selectedOption === "text" ? (
+  //               <div className="search-content">
+  //                 <input
+  //                   className="form-control mr-2"
+  //                   type="text"
+  //                   id="search-input"
+  //                   placeholder="Search"
+  //                 />
+  //               </div>
+  //             ) : selectedOption === "image" ? (
+  //               <div className="search-content">
+  //                 <UploadInput label="Upload Image" />
+  //               </div>
+  //             ) : (
+  //               <div className="search-content">
+  //                 <UploadInput label="Upload Video" />
+  //               </div>
+  //             )}
+  //             <button
+  //               className="btn search-btn ml-2"
+  //               type="button"
+  //               onClick={onSearch}
+  //             >
+  //               <FontAwesomeIcon icon={faSearch} color="#fff" />
+  //             </button>
+  //           </div> */}
+  //             <div className="search-input">
+  //               <div className="search-input-input">
+  //                 <input type="text" placeholder="Enter Search Term" />
+  //                 <HeadingTwo text="or" />
+  //                 <button className="btn  btn-color-white-one">
+  //                   <FontAwesomeIcon icon={faUpload} /> Upload File
+  //                 </button>
+  //               </div>
 
-                <div className="search-input-checkbox">
-                  <BodyOne text="include" />
-                  <label className="checkbox-box">
-                    <BodyOne text="text" />
-                    <input
-                      type="checkbox"
-                      onClick={() => this.checkboxToggle("text")}
-                    />
-                    <span className="checkmark" />
-                  </label>
-                  <label className="checkbox-box">
-                    <BodyOne text="image" />
-                    <input
-                      type="checkbox"
-                      onClick={() => this.checkboxToggle("image")}
-                    />
-                    <span className="checkmark" />
-                  </label>
-                  <label className="checkbox-box">
-                    <BodyOne text="video" />
-                    <input
-                      type="checkbox"
-                      onClick={() => this.checkboxToggle("video")}
-                    />
-                    <span className="checkmark" />
-                  </label>
-                </div>
-                <button
-                  className="btn btn-color-primary-one text-white"
-                  type="submit"
-                  onClick={this.onFormSubmit.bind(this)}
-                >
-                  Search <FontAwesomeIcon icon={faSearch} color="#fff" />
-                </button>
-              </div>
-            </div>
-            <div className="form-inline mt-3">
-              <label className="checkbox-box">
-                text
-                <input type="checkbox" onClick={() => checkboxToggle("text")} />
-                <span className="checkmark" />
-              </label>
-              <label className="checkbox-box">
-                image
-                <input
-                  type="checkbox"
-                  onClick={() => checkboxToggle("image")}
-                />
-                <span className="checkmark" />
-              </label>
-              <label className="checkbox-box">
-                video
-                <input
-                  type="checkbox"
-                  onClick={() => checkboxToggle("video")}
-                />
-                <span className="checkmark" />
-              </label>
-            </div>
-          </form>
-        </div>
-        <div className="search-result container mt-5">
-          {/* empty cards for loading */}
-          {/* {props.fetch.loading ? <Loading /> : null} */}
-          {/* spinner for loading */}
-          {props.fetch.loading ? <Spinner /> : null}
-          <div className="card-columns">{displayResults([props.fetch])}</div>
-        </div>
-      </div>
-    );
-  } else {
-    props.error("Permission denied");
-    return null;
-  }
+  //               <div className="search-input-checkbox">
+  //                 <BodyOne text="include" />
+  //                 <label className="checkbox-box">
+  //                   <BodyOne text="text" />
+  //                   <input
+  //                     type="checkbox"
+  //                     onClick={() => this.checkboxToggle("text")}
+  //                   />
+  //                   <span className="checkmark" />
+  //                 </label>
+  //                 <label className="checkbox-box">
+  //                   <BodyOne text="image" />
+  //                   <input
+  //                     type="checkbox"
+  //                     onClick={() => this.checkboxToggle("image")}
+  //                   />
+  //                   <span className="checkmark" />
+  //                 </label>
+  //                 <label className="checkbox-box">
+  //                   <BodyOne text="video" />
+  //                   <input
+  //                     type="checkbox"
+  //                     onClick={() => this.checkboxToggle("video")}
+  //                   />
+  //                   <span className="checkmark" />
+  //                 </label>
+  //               </div>
+  //               <button
+  //                 className="btn btn-color-primary-one text-white"
+  //                 type="submit"
+  //                 onClick={this.onFormSubmit.bind(this)}
+  //               >
+  //                 Search <FontAwesomeIcon icon={faSearch} color="#fff" />
+  //               </button>
+  //             </div>
+  //           </div>
+  //           <div className="form-inline mt-3">
+  //             <label className="checkbox-box">
+  //               text
+  //               <input type="checkbox" onClick={() => checkboxToggle("text")} />
+  //               <span className="checkmark" />
+  //             </label>
+  //             <label className="checkbox-box">
+  //               image
+  //               <input
+  //                 type="checkbox"
+  //                 onClick={() => checkboxToggle("image")}
+  //               />
+  //               <span className="checkmark" />
+  //             </label>
+  //             <label className="checkbox-box">
+  //               video
+  //               <input
+  //                 type="checkbox"
+  //                 onClick={() => checkboxToggle("video")}
+  //               />
+  //               <span className="checkmark" />
+  //             </label>
+  //           </div>
+  //         </form>
+  //       </div>
+  //       <div className="search-result container mt-5">
+  //         {/* empty cards for loading */}
+  //         {/* {props.fetch.loading ? <Loading /> : null} */}
+  //         {/* spinner for loading */}
+  //         {props.fetch.loading ? <Spinner /> : null}
+  //         <div className="card-columns">{displayResults([props.fetch])}</div>
+  //       </div>
+  //     </div>
+  //   );
+  // } else {
+  //   props.error("Permission denied");
+  //   return null;
+  // }
 }
 
 SearchInput.propTypes = {
