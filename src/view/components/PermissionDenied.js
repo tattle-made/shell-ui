4ab3f1911712b586d2d2b7e8ec784a1daa15f324
mohit1604 <malikmohit619@gmail.com>
2019-10-03 02:34:38 +0530

    removing sambha gif and funny text for permission denied page

--- src/view/components/PermissionDenied.js
+++ src/view/components/PermissionDenied.js
@@ -1,24 +1,17 @@
 import React, { Component } from 'react';
 import HeadingOne from '../atomic-components/text/HeadingOne';
+import { Card } from 'react-bootstrap';
 
 class PermissionDenied extends Component {
   render() {
     return (
-      <div className='text-center text-white pt-5' style={styles}>
-        <HeadingOne text='Aareee OO Sambha!' />
-        <HeadingOne text='Permission hai kya tumree paas !' />
-        <img
-          src='https://i.giphy.com/media/l1IY5q3y8F41XWNwY/giphy.webp'
-          alt='permission denied'
-        />
+      <div className='permission-denied'>
+        <div className='permission-denied-box'>
+          <HeadingOne text='Permission Denied' />
+        </div>
       </div>
     );
   }
 }
 
-const styles = {
-  backgroundImage: `url(https://media.giphy.com/media/xVn3ZmKrKIOLS/giphy.gif)`,
-  width: '100vw',
-  height: '100vh'
-};
 export default PermissionDenied;
