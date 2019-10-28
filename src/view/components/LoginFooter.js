337389b4aa461aa905385daa7b70843c45b2c475
denny <denny.george90@gmail.com>
2019-10-28 16:22:11 -0700

    incorporated version number on the ui

--- src/view/components/LoginFooter.js
+++ src/view/components/LoginFooter.js
@@ -1,56 +1,64 @@
 import React, { Component } from "react";
 import HeadingThree from "../atomic-components/text/HeadingThree";
-import BodyOne from "../atomic-components/text/BodyOne";
+import BodyTwo from "../atomic-components/text/BodyTwo";
+import packageJsonFile from '../../../package.json';
+
+const {version} = packageJsonFile;
 
 class LoginFooter extends Component {
   render() {
     return (
-      <div className="login-footer">
-        {/* <div>
-          <HeadingThree text="Reach Out" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-        </div>
-        <div>
-          <HeadingThree text="Social" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-        </div>
-        <div>
-          <HeadingThree text="Values" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-        </div>
-        <div>
-          <HeadingThree text="Values" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-          <BodyOne text="facebook" />
-        </div> */}
+      <div>
+        <br/><br/><br/>
+        <BodyTwo text={`version : ${version}`} />
       </div>
+      // <div className="login-footer">
+      //   {/* <div>
+      //     <HeadingThree text="Reach Out" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //   </div>
+      //   <div>
+      //     <HeadingThree text="Social" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //   </div>
+      //   <div>
+      //     <HeadingThree text="Values" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //   </div>
+      //   <div>
+      //     <HeadingThree text="Values" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //     <BodyOne text="facebook" />
+      //   </div> */}
+        
+      // </div>
     );
   }
 }
