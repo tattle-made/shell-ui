2369be54d38547bd1ca035d8d1f19dc68603979a
denny <denny.george90@gmail.com>
2019-10-28 15:48:31 -0700

    fixed video preview to fetch video url of the correct source

--- src/view/components/PreviewFormatterTable.js
+++ src/view/components/PreviewFormatterTable.js
@@ -9,6 +9,14 @@ const imageSource = (userId, fileName) => {
   }
 }
 
+const videoSource = (userId, fileName) => {
+  if(userId===159){
+    return `https://tattle-services.s3.ap-south-1.amazonaws.com/${fileName}`
+  }else{
+    return `https://firebasestorage.googleapis.com/v0/b/crowdsourcesocialposts.appspot.com/o/bot-posts%2F${fileName}?alt=media&token=88192814-45bb-4302-b409-b5c26e90390b`
+  }
+}
+
 class PreviewFormatterTable extends Component {
   render() {
     if (this.props.row.type === 'image') {
@@ -21,17 +29,17 @@ class PreviewFormatterTable extends Component {
         </div>
       );
     } else if (this.props.row.type === 'video') {
-      // <video
-      //   // src={`https://firebasestorage.googleapis.com/v0/b/crowdsourcesocialposts.appspot.com/o/bot-posts%2F${cell}?alt=media&token=88192814-45bb-4302-b409-b5c26e90390b`}
-      //   // src={"https://youtu.be/DBXH9jJRaDk"}
-      // />
+      return (<video
+        src={videoSource(this.props.row.user_id, this.props.cell)}
+        controls
+      />)
 
-      return (
-        <iframe title='d' src='https://www.youtube.com/embed/hZFNVj505HQ' />
-        // <div className='card'>
-        //   <iframe title='d' src='https://www.youtube.com/embed/hZFNVj505HQ' />
-        // </div>
-      );
+      // return (
+      //   <iframe title='d' src='https://www.youtube.com/embed/hZFNVj505HQ' />
+      //   // <div className='card'>
+      //   //   <iframe title='d' src='https://www.youtube.com/embed/hZFNVj505HQ' />
+      //   // </div>
+      // );
     } else if (this.props.row.type === 'text') {
       return (
         <div className='card'>
