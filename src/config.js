fdc8f000064982b6179f390d632c8834157d222b
denny <denny.george90@gmail.com>
2020-02-09 17:48:05 +0530

    (chore) rename api endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-//development
+// development
 // export const ARCHIVE_SERVER_PATH = 'http://localhost:3003';
 
-//production
-export const ARCHIVE_SERVER_PATH = 'http://archive-staging.ap-south-1.elasticbeanstalk.com:3003';
+// production
+export const ARCHIVE_SERVER_PATH = 'https://archive.tattle.co.in';
