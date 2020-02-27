9f74769a023986c58af78a24c2ccf0672fe8dbbd
denny <denny.george90@gmail.com>
2020-02-27 15:06:41 +0530

    (feat) changed endpoint to dev

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@
 // export const ARCHIVE_SERVER_PATH = 'http://localhost:3003';
 
 // production
-export const ARCHIVE_SERVER_PATH = 'https://archive.tattle.co.in';
+export const ARCHIVE_SERVER_PATH = 'https://archive-server-dev.tattle.co.in';
